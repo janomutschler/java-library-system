78145e412c1d6d3018ceac0d7d4fef50bee45dcc
Jano <jano.mutschler@outlook.com>
2026-04-20 08:31:11 +0200

    added first book model

---
+++ src/model/Book.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.Objects;
+
+public class Book implements Comparable<Book> {
+
+    private final String id;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean available;
+
+    public Book(String id, String title, String author, String genre, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.available = available;
+    }
+
+    // --- Getters ---
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               "; Title: " + title +
+               "; Author: " + author +
+               "; Genre: " + genre +
+               "; Available: " + available;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Book other = (Book) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        // Sort by title (case-insensitive)
+        return this.title.compareToIgnoreCase(other.title);
+    }
+}
