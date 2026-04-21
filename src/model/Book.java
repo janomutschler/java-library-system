2db23a79e8aef793c47066c82e306e9a8aa60182
Jano <jano.mutschler@outlook.com>
2026-04-21 18:27:11 +0200

    added borrow date and due date to Book class

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Objects;
+import java.util.GregorianCalendar;
 
 public class Book implements Comparable<Book> {
 
@@ -9,6 +10,8 @@ public class Book implements Comparable<Book> {
     private final String author;
     private final String genre;
     private boolean available;
+    private GregorianCalendar borrowDate;
+    private GregorianCalendar dueDate;
 
     public Book(int id, String title, String author, String genre, boolean available) {
         this.id = id;
@@ -16,8 +19,11 @@ public class Book implements Comparable<Book> {
         this.author = author;
         this.genre = genre;
         this.available = available;
+        this.borrowDate = null;
+        this.dueDate = null;
     }
 
+    //getter
     public int getId() {
         return id;
     }
@@ -38,23 +44,49 @@ public class Book implements Comparable<Book> {
         return available;
     }
 
+    public GregorianCalendar getBorrowDate() {
+        return borrowDate;
+    }
+
+    public GregorianCalendar getDueDate() {
+        return dueDate;
+    }
+
+    //setter
     public void setAvailable(boolean available) {
         this.available = available;
     }
 
-	@Override
-	public String toString() {
-		String status = available ? "Available" : "Borrowed";
+    public void setBorrowDate(GregorianCalendar borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+    public void setDueDate(GregorianCalendar dueDate) {
+        this.dueDate = dueDate;
+    }
 
-		return String.format(
-			"%-5d %-20s | %-20s | %-15s | %s",
-			id,
-			title,
-			author,
-			genre,
-			status
-		);
-	}
+    @Override
+    public String toString() {
+        String status = available ? "Available" : "Borrowed";
+
+        String dateInfo = "";
+        if (borrowDate != null && dueDate != null) {
+            dateInfo = String.format(
+                " | Borrowed: %tF | Due: %tF",
+                borrowDate,
+                dueDate
+            );
+        }
+
+        return String.format(
+            "%-5d %-20s | %-20s | %-15s | %s%s",
+            id,
+            title,
+            author,
+            genre,
+            status,
+            dateInfo
+        );
+    }
 
     @Override
     public boolean equals(Object obj) {
